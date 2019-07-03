16fa08677037ac58f2ec6b25d53039d4f34b65e9
Jacek Kucharczyk <kucjac@gmail.com>
2019-07-03 13:11:14 +0200

    minor fix in jsonapi

--- encoding/jsonapi/common.go
+++ encoding/jsonapi/common.go
@@ -1,11 +1,5 @@
 package jsonapi
 
-import (
-	"github.com/neuronlabs/neuron-core/internal/controller"
-)
-
-var c = controller.Default()
-
 const (
 	// MediaType is the identifier for the JSON API media type
 	// see http://jsonapi.org/format/#document-structure
